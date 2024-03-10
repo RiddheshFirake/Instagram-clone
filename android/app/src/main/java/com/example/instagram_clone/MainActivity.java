251ebe7a0581a8c6ab703796c9820b72f96990a4
riddh <riddhesh.firake@gmail.com>
2024-03-10 21:35:27 +0530

    error fixed

--- android/app/src/main/java/com/example/instagram_clone/MainActivity.java
+++ android/app/src/main/java/com/example/instagram_clone/MainActivity.java
@@ -1,6 +1,6 @@
-package com.example.instagram_clone;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
+//package com.example.instagram_clone;
+//
+//import io.flutter.embedding.android.FlutterActivity;
+//
+//public class MainActivity extends FlutterActivity {
+//}
